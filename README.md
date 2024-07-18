8df8880c4acb350788d16e5ea6cf97acd1d524cb
YeseniaLopez <ylopez.amarisext@sistecredito.com>
2024-07-18 09:36:52 -0500

    primeros pasos del api

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Este proyecto es aprendizaje de Nest.js con GraphQL conectado a Base de datos postgres. Se construyó en el curso de udemy [Nest + GraphQL: Evoluciona tus APIs]()
 
 NOTAS: [Curso GraphQL en Nest.js](https://www.notion.so/Curso-GraphQL-en-Nest-js-0b269d976e1549b09af47b5c6abb512c?pvs=4)
+
+
+# Inicializar proyecto en Dev
+
+1. Clonar el proyecto 
+2. Copiar el ```env.template``` y renombrar a ```.env``` y agregar la DB_PASSWORD
+3. Ejecutar ```npm install```
+4. Levantar la imagen (Con Docker desktop inicializado) ```docker-compose up -d```
+5. Levantar el backend de Nest.js ```npm start:dev```
+6. Disponible en [localhost:3000/graphql](localhost:3000/graphql)
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    ItemsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
